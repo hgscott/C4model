169268bae88d5d4c3af423ab922fbb3b6a3b3316
hgscott <helen.scott@ttu.edu>
2020-04-12 12:29:42 -0400

    Update calc_cbs.R

--- functions/calc_cbs.R
+++ functions/calc_cbs.R
@@ -10,9 +10,9 @@ calc_cbs <- function(z, Al, vpmax, cm) {
   Rd <- 0.04 * vpmax # Leaf mitochondrial respiration, 4 coming from rounding 3.9 or 4.4 % from the ecology LCE paper
   Rm <- 0.5 * Rd
   An <- A_gross - Rm
-  CCM <- ((vpmax - An)/gbs) # micromol mol^-1
+  CCM <- ((vpmax - An)/gbs) # micromol mol^-1 # NEEDS ADJUSTED
   
-  CCM <- CCM * 1e-6 * patm # Convert to Pa
+  CCM <- CCM * 1e-6 * patm # Convert to Pa # NEEDS ADJUSTED
   
   Cbs <- cm + CCM
   return(Cbs)
